52e1a4812f7ac62e8a1295a6765cad22ab9c4773
Problematique <problematique@deeplay.io>
2021-03-24 22:57:39 +0700

    lector about finding elements by Get, Find, Eq

---
+++ cypress/integration/firsttest.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+
+context('Todos', function () {
+    it('should show #main', function () {
+        cy.get('.main').should('be.visible')
+        cy.contains('Hello')
+    })
+})
